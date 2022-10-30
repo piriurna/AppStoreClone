8673c28d8fd896798ef7c5cd24e6a4649351c22b
Franco <francozalamena@gmail.com>
2022-10-30 14:50:42 +0000

    Main Fragment

--- app/src/main/java/com/piriurna/appstoreclone/ui/main/MainFragment.kt
+++ app/src/main/java/com/piriurna/appstoreclone/ui/main/MainFragment.kt
@@ -1,12 +1,15 @@
 package com.piriurna.appstoreclone.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.piriurna.appstoreclone.R
+import com.piriurna.appstoreclone.databinding.FragmentMainBinding
+import com.piriurna.appstoreclone.ui.main.adapters.LocalTopAppsAdapter
+import com.piriurna.domain.models.App
 
 class MainFragment : Fragment() {
 
@@ -14,19 +17,23 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
+    private val viewModel: MainViewModel by viewModels()
+
+    private lateinit var binding : FragmentMainBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.localTopApps.adapter = LocalTopAppsAdapter(App.mockApps)
+    }
 }
--- app/src/main/java/com/piriurna/domain/models/App.kt
+++ app/src/main/java/com/piriurna/domain/models/App.kt
@@ -20,4 +20,60 @@ data class App(
     val versionName: String
 ){
 
+
+    companion object {
+        val mockApps = listOf(
+            App(
+                id = 0,
+                name = "App 0",
+                size = 100,
+                added = "123123",
+                apkTags = listOf("app", "test", "0"),
+                downloads = 1,
+                graphic = "https://pool.img.aptoide.com/appupdater/e5a151578bdc332c41c6708e2e680106.png",
+                icon = "https://pool.img.aptoide.com/appupdater/df4fd24162f8caec8e487cbb423c29cc_icon.png",
+                modified = "123123",
+                rating = 4.9,
+                storeId = 0,
+                storeName = "App Test 0",
+                updated = "123123",
+                versionCode = 1,
+                versionName = "1.0"
+            ),
+            App(
+                id = 1,
+                name = "App 1",
+                size = 100,
+                added = "123123",
+                apkTags = listOf("app", "test", "1"),
+                downloads = 1,
+                graphic = "https://pool.img.aptoide.com/pixl/a2ef54d268050ab5388238267931635d.png",
+                icon = "https://pool.img.aptoide.com/pixl/b53365086c49e162fdb442636638cce8_icon.png",
+                modified = "123123",
+                rating = 3.2,
+                storeId = 0,
+                storeName = "App Test 1",
+                updated = "123123",
+                versionCode = 1,
+                versionName = "1.0"
+            ),
+            App(
+                id = 2,
+                name = "App 2",
+                size = 100,
+                added = "123123",
+                apkTags = listOf("app", "test", "2"),
+                downloads = 1,
+                graphic = "https://pool.img.aptoide.com/appupdater/06b85cc01963bc0403ba61e08f52384d_fgraphic.jpg",
+                icon = "https://pool.img.aptoide.com/appupdater/93e2cb0e9da333f9d9658ddcb4825c22_icon.png",
+                modified = "123123",
+                rating = 4.2,
+                storeId = 0,
+                storeName = "App Test 2",
+                updated = "123123",
+                versionCode = 1,
+                versionName = "1.0"
+            )
+        )
+    }
 }
