6f843596cb84c2f43d1bfa45438bd6435df4101b
Franco <francozalamena@gmail.com>
2022-10-30 14:49:54 +0000

    LocalTopAppsAdapter

---
+++ app/src/main/java/com/piriurna/appstoreclone/ui/main/adapters/LocalTopAppsAdapter.kt
@@ -0,0 +1,50 @@
+package com.piriurna.appstoreclone.ui.main.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.net.toUri
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.piriurna.appstoreclone.R
+import com.piriurna.domain.models.App
+
+class LocalTopAppsAdapter(private val items : List<App>) : RecyclerView.Adapter<LocalTopAppsAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.asc_app_card_layout, viewGroup, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        with(items[position]) {
+            viewHolder.appName.text = name
+            viewHolder.appRating.text = rating.toString()
+            viewHolder.appImage.load(this.icon)
+        }
+    }
+
+
+    override fun getItemCount() = items.size
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val appName: TextView
+
+        val appRating : TextView
+
+        val appImage : ImageView
+
+        init {
+            // Define click listener for the ViewHolder's View.
+            appName = view.findViewById(R.id.txt_app_name)
+
+            appRating = view.findViewById(R.id.txt_rating)
+
+            appImage = view.findViewById(R.id.img_app_image)
+        }
+    }
+}
