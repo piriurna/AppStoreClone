42932e834861ae5a4c48a8b39271017c2e6c6e9f
Franco <francozalamena@gmail.com>
2022-10-31 13:06:02 +0000

    NavController

--- app/src/main/java/com/piriurna/appstoreclone/MainActivity.kt
+++ app/src/main/java/com/piriurna/appstoreclone/MainActivity.kt
@@ -2,6 +2,9 @@ package com.piriurna.appstoreclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.piriurna.appstoreclone.ui.main.MainFragment
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -11,10 +14,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        toolbar.setupWithNavController(navController)
     }
 }
